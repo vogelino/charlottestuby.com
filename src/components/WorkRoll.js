26677b76aea16352def252dfbed9317f233293ef
Lucas Vogel <contact@vogelino.com>
2020-10-19 22:07:56 +0200

    Add work template key

--- src/components/WorkRoll.js
+++ src/components/WorkRoll.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import { Link, graphql, StaticQuery } from 'gatsby'
 import PreviewCompatibleImage from './PreviewCompatibleImage'
 
-class BlogRoll extends React.Component {
+class WorkRoll extends React.Component {
   render() {
     const { data } = this.props
     const { edges: posts } = data.allMarkdownRemark
@@ -58,7 +58,7 @@ class BlogRoll extends React.Component {
   }
 }
 
-BlogRoll.propTypes = {
+WorkRoll.propTypes = {
   data: PropTypes.shape({
     allMarkdownRemark: PropTypes.shape({
       edges: PropTypes.array,
@@ -69,10 +69,10 @@ BlogRoll.propTypes = {
 export default () => (
   <StaticQuery
     query={graphql`
-      query BlogRollQuery {
+      query WorkRollQuery {
         allMarkdownRemark(
-          sort: { order: DESC, fields: [frontmatter___date] }
-          filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
+          sort: { order: DESC, fields: [orderOfAppearance] }
+          filter: { frontmatter: { templateKey: { eq: "work" } } }
         ) {
           edges {
             node {
@@ -82,7 +82,9 @@ export default () => (
                 slug
               }
               frontmatter {
+                orderOfAppearance
                 title
+                subtitle
                 templateKey
                 date(formatString: "MMMM DD, YYYY")
                 featuredpost
@@ -99,6 +101,6 @@ export default () => (
         }
       }
     `}
-    render={(data, count) => <BlogRoll data={data} count={count} />}
+    render={(data, count) => <WorkRoll data={data} count={count} />}
   />
 )
