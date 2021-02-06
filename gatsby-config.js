81c850d7bc870a1e958d82f09be7f0976dbad958
Lucas Vogel <contact@vogelino.com>
2021-02-06 12:37:38 +0100

    build(cms-config): update reponame in config.yml

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 const siteMetadata = {
-	title: 'Portfolio of Charlotte Stuby',
-	description: 'A swiss artist based in Brussels specialized in textile work',
+	title: 'Portfolio website of Charlotte Stuby',
+	description:
+		'Charlotte Stuby is a swiss artist based in Brussels. Her practice focuses mainly on objects and installations produced by varied materials such as concrete, wood, metal but mostly sewing and embroidery. This is the Github repository of her portfolio website.',
 	keywords: [
 		'Charlotte',
 		'Stuby',
